41ce122039b8d3efa455a7a2f4e880d0ac3f5592
Kate Brown <kbrown8158@gmail.com>
2017-03-09 20:10:58 -0500

    New problem in Ruby

---
+++ main.js
@@ -0,0 +1,16 @@
+// If we list all the natural numbers below 10 that are multiples of 3 or 5,
+// we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+// Find the sum of all the multiples of 3 or 5 below 1000.
+
+let sum;
+
+for ( let count = 1, count <= 10, i++)
+  {
+    if (count % 3 == 0)
+      sum += count
+    else if (count % 5 == 0)
+      sum += count
+  }
+
+  return sum
